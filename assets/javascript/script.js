a88892e2f0d177e37933dfa69b788f4b611c8d7f
Berlic <bsorrell3@gmail.com>
2021-07-25 20:17:57 -0400

    swapped over to the provided starter code..oops

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,31 @@
-var displayDate= $("#displayDate");
+var displayDate= $("#currentDay");
 var date=moment();
-
+var timeIDEl= $(".timeID");
+var inputField= $(".description");
+// ..........Display Current Date.............//
 displayDate.text(date.format("LLLL"));
 
-var timeIDEl= $(".timeID");
-var inputField= $(".form-control");
-
-console.log(inputField);
-
-
+//..........Change Input Background Based On Time..........//
 timeIDEl.each(function(index){
     var timeID = timeIDEl[index].innerHTML
     var mTimeID = moment(timeID, "h hh a");
     
     if(mTimeID.hour() < date.hour()){
-        $(inputField[index]).css("background-color","lightgray");
+        $(inputField[index]).toggleClass("past");
     }
     else if(mTimeID.hour() == date.hour()){
-        $(inputField[index]).css({"background-color":"Tomato", "color":"white"});
+        $(inputField[index]).toggleClass("present");
     }
     else{
-        $(inputField[index]).css("background-color","DarkSeaGreen");
+        $(inputField[index]).toggleClass("future");
     };
 });
+
+//..........Display Items From Current Local Storage........//
+var currentList = localStorage.ToDo;
+
+if(currentList == undefined){
+    localStorage.ToDo = JSON.stringify([]);
+}
+
+console.log(JSON.parse(localStorage.ToDo));
